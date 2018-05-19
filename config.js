19ed49c15a13617ab3966b9955b31371260937c5
leo.cim <leo.cimetiere@hotmail.fr>
2018-05-19 17:10:14 +0200

    heroku deploy3

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Connector
 module.exports = {
-    cookie: '_ga=GA1.2.1452027106.1516909406; lastMarksViewSelected=table; _gid=GA1.2.1311904915.1526453935; SESSION=d0e13f1a-8492-46e8-9794-62d88e3f5e54; XSRF-TOKEN=083c7dec-1122-4bf5-811f-67bca34c7283; _gat=1',
+    cookie: '_ga=GA1.2.1452027106.1516909406; lastMarksViewSelected=table; _gid=GA1.2.1349076014.1526658577; _gat=1; SESSION=5aae0432-bda5-4152-98ea-1a7973e90424; XSRF-TOKEN=c111a34b-998c-4012-a3e8-ad9c867c4aff',
     connectorAppId: process.env.MICROSOFT_APP_ID,
     connectorAppPassword: process.env.MICROSOFT_APP_PASSWORD,
     luisModelUrl: 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/f861946d-9b6c-47ee-8ebf-37cdbf692a3c?subscription-key=6967f83834db40b4b63f2d565183ee76&verbose=true&timezoneOffset=0&q='
